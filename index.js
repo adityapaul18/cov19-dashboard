f40672253cca158eeb503f148ebedfe350970699
adityapaul18 <62583380+adityapaul18@users.noreply.github.com>
2020-08-11 19:28:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,24 +31,24 @@ var t=document.getElementById("t1");
 
 t.click();
 
-var y1=document.getElementById("dcc")
-var y2=document.getElementById("dca")
-var y3=document.getElementById("dcr")
-var y4=document.getElementById("dcd")
-
-var x1=document.getElementById("cc")
-var x2=document.getElementById("ca")
-var x3=document.getElementById("cr")
-var x4=document.getElementById("cd")
-
-var x,y,z,a,b,c,xx,yy;
 
 async function fetchWW()
 {
     res=await fetch('https://api.covid19api.com/summary');
     src=await res.json();
-  
-    console.log(src.Countries);
+    
+    console.log(src);
+    var y1=document.getElementById("dcc")
+    var y2=document.getElementById("dca")
+    var y3=document.getElementById("dcr")
+    var y4=document.getElementById("dcd")
+    
+    var x1=document.getElementById("cc")
+    var x2=document.getElementById("ca")
+    var x3=document.getElementById("cr")
+    var x4=document.getElementById("cd")
+    
+    var x,y,z,a,b,c,xx,yy;
     x=src.Global.NewConfirmed;
     y=src.Global.NewRecovered;
     xx=src.Global.TotalDeaths
@@ -60,7 +60,7 @@ async function fetchWW()
     x1.innerHTML=a+x;
     x2.innerHTML=c+z;
     x3.innerHTML=b+y;
-    x4.innerHTML=xx;
+    x4.innerHTML='+'+xx;
 
     y1.innerHTML='+'+x;
     y2.innerHTML=z;
@@ -95,3 +95,47 @@ updatetable=(countries)=>{
         $("#covtab").DataTable();
     }
 fetchWc();
+
+var faridabad=[];
+
+async function fetchIW()
+{
+    res=await fetch('https://api.covid19india.org/state_district_wise.json');
+    src=await res.json();
+    faridabad=src.Haryana.districtData.Faridabad;
+    console.log(faridabad);
+
+    var y1=document.getElementById("dccf")
+    var y2=document.getElementById("dcaf")
+    var y3=document.getElementById("dcrf")
+    var y4=document.getElementById("dcdf")
+    
+    var x1=document.getElementById("ccf")
+    var x2=document.getElementById("caf")
+    var x3=document.getElementById("crf")
+    var x4=document.getElementById("cdf")
+    
+    var x,y,z,a,b,c,xx,yy;
+    x=faridabad.delta.confirmed;
+    y=faridabad.delta.recovered;
+    yy=faridabad.delta.deceased;
+    xx=faridabad.deceased;
+    a=faridabad.confirmed;
+    b=faridabad.recovered;
+    c=a-b-xx;
+    z=x-y-yy;
+    x1.innerHTML=a+x;
+    x2.innerHTML=c+z;
+    x3.innerHTML=b+y;
+    x4.innerHTML=xx;
+
+    y1.innerHTML='+'+x;
+    y2.innerHTML=z;
+    y3.innerHTML='+'+y;
+    y4.innerHTML='+'+yy;
+
+    
+}
+
+fetchIW();
+
